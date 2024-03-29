19751774ff93bc52db6afbd64447ab5d2c7a468e
desalegn kassa <desalegnashagrie@gmail.comgit config user.emailgit config user.emailgit config --global user.email desalegnashagrie@gmail.com>
2024-03-28 23:45:18 -0500

    first config commit

---
+++ src/test/java/com/ConfigServerAgain/ConfgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ConfigServerAgain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
